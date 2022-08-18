efcf4168d3de50f7edca0c7db9c7dc140ecc2d00
Aqua <superhacker@z.ru>
2022-08-18 18:17:08 +0300

    added shebang in script

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# q1ncite official project, subsrcibe my youtube channel
 list_name = input('Names: ').split(', ')
 phone_numbers_list = input('Phone numbers: ').replace(',', ', ').split(', ')
 zipped = zip(list_name, phone_numbers_list)
